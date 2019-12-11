5abd17b93b474852d88f9abb64e9082872b78ba1
Ubuntu <ubuntu@ip-172-31-40-60.ec2.internal>
2019-12-11 15:05:16 +0000

    Add an if statement in the add to cart view

--- cart/views.py
+++ cart/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 def view_cart(request):
-    """A View that renders all the contents within the cart, on the cart contents page"""
+    """A View that renders the cart contents page"""
     return render(request, "cart.html")
-    
-    
+
+
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
     return redirect(reverse('index'))
-    
-    
+
+
 def adjust_cart(request, id):
     """
     Adjust the quantity of the specified product to the specified
