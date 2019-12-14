ccff26e9b6e85bb840a1fa51c7c34f0d8064c373
Ubuntu <ubuntu@ip-172-31-40-60.ec2.internal>
2019-12-14 16:17:35 +0000

    Change the region to eu-north-1 for AWS S3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,7 +145,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_STORAGE_BUCKET_NAME = 'livias-ecommerce'
-AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
