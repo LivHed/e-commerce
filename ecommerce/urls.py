c38919aee115631f8d4cf46dccb16e63704068e3
Ubuntu <ubuntu@ip-172-31-40-60.ec2.internal>
2019-12-15 12:45:22 +0000

    Update typo

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,6 +24,7 @@ from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', all_products, name='index'),
